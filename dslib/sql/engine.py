f370ede5550009ec8a469fa98faa6fa440f74931
Shweta Chopra <shweta.shwetachopra@gmail.com>
2022-10-18 13:29:05 -0700

    Update engine.py

--- dslib/sql/engine.py
+++ dslib/sql/engine.py
@@ -1,105 +1,3 @@
-"""
-Get an appropriate BlueLabs Joblib or SQL Alchemy Engine
-"""
-import getpass
-import os
-import warnings
-
-# with warnings.catch_warnings():
-#     warnings.filterwarnings("ignore")
-#     from records_mover import Session
-
-import sqlalchemy as sa
-
-options = dict()
-options['default_database'] = 'default'
-
-def set_engine(creds='default'):
-    options['default_database'] = creds
-
-def get_engine(creds=None, verbose=False):
-    """
-
-    Args:
-        creds ():
-        verbose ():
-
-    https://docs.sqlalchemy.org/en/latest/core/engines.html
-
-    Returns:
-
-    """
-
-    if creds == None:
-        creds = options['default_database']
-
-    # session = Session()
-    # if creds == 'default':
-    #     return session.get_default_db_engine()
-    if getpass.getuser() == 'jovyan':
-        if creds == 'dnc':
-            driver = 'vertica+vertica_python'
-            username = os.environ.get('DNC_DB_USERNAME')
-            password = os.environ.get('DNC_DB_PASSWORD')
-            host = os.environ.get('DNC_DB_HOST')
-            port = os.environ.get('DNC_DB_PORT')
-            database = os.environ.get('DNC_DB_DATABASE')
-            return sa.create_engine(
-                f"{driver}://{username}:{password}@{host}:{port}/{database}",
-                echo=verbose
-            )
-        elif creds == 'redshift':
-            driver = 'redshift'
-            username = os.environ.get('REDSHIFT_DB_USERNAME')
-            password = os.environ.get('REDSHIFT_DB_PASSWORD')
-            host = os.environ.get('REDSHIFT_DB_HOST')
-            port = os.environ.get('REDSHIFT_DB_PORT')
-            database = os.environ.get('REDSHIFT_DB_DATABASE')
-            return sa.create_engine(
-                f"{driver}://{username}:{password}@{host}:{port}/{database}",
-                echo=verbose
-            )
-        elif creds == 'influencers':
-            driver = 'redshift'
-            username = os.environ.get('INFLUENCERS_DB_USERNAME')
-            password = os.environ.get('INFLUENCERS_DB_PASSWORD')
-            host = os.environ.get('INFLUENCERS_DB_HOST')
-            port = os.environ.get('INFLUENCERS_DB_PORT')
-            database = os.environ.get('INFLUENCERS_DB_DATABASE')
-            return sa.create_engine(
-                f"{driver}://{username}:{password}@{host}:{port}/{database}",
-                echo=verbose
-            )
-        elif creds == 'postgres':
-            driver = 'postgresql'
-            username = os.environ.get('POSTGRES_DB_USERNAME')
-            password = os.environ.get('POSTGRES_DB_PASSWORD')
-            host = os.environ.get('POSTGRES_DB_HOST')
-            port = os.environ.get('POSTGRES_DB_PORT')
-            database = os.environ.get('POSTGRES_DB_DATABASE')
-            return sa.create_engine(
-                f"{driver}://{username}:{password}@{host}:{port}/{database}",
-                echo=verbose
-            )
-        elif creds == 'default':
-            driver = 'redshift'
-            username = os.environ.get('DB_USERNAME')
-            password = os.environ.get('DB_PASSWORD')
-            host = os.environ.get('DB_HOST')
-            port = os.environ.get('DB_PORT')
-            database = os.environ.get('DB_DATABASE')
-            return sa.create_engine(
-                f"{driver}://{username}:{password}@{host}:{port}/{database}",
-                echo=verbose
-            )
-
-    else:
-        raise NotImplementedError("get_engine() not implemented of these credentials.")
-
-
-
-"""
-In case you need to reference this to make your own engine at some point:
 import sqlalchemy as sa
 import os
 
@@ -118,12 +16,12 @@ def get_engine(creds='default', verbose=False):
         host = os.environ.get('DB_HOST')
         port = os.environ.get('DB_PORT')
         database = os.environ.get('DB_DATABASE')
+        driver = os.environ.get('DB_DRIVER')
 
     else:
         raise NotImplementedError
 
     return sa.create_engine(
-        f"redshift://{username}:{password}@{host}:{port}/{database}",
+        f"{driver}://{username}:{password}@{host}:{port}/{database}",
         echo=verbose
     )
-"""
